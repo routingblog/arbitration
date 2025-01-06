551b0b71651257c92675ef4ef1d6f7570729345b
Eitan Adler <lists@eitanadler.com>
2025-01-06 07:31:58 -0800

    [site] add general audit to go.zsh

--- go.zsh
+++ go.zsh
@@ -3,3 +3,5 @@
 pre-commit run
 markdownlint -c .markdownlint.jsonc content
 vale content
+# https://gohugo.io/troubleshooting/audit/
+HUGO_MINIFY_TDEWOLFF_HTML_KEEPCOMMENTS=true HUGO_ENABLEMISSINGTRANSLATIONPLACEHOLDERS=true hugo && grep -inorE "<\!-- raw HTML omitted -->|ZgotmplZ|\[i18n\]|\(<nil>\)|(&lt;nil&gt;)|hahahugo" public/
