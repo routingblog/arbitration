10e4db48f6d600dab6adbf546bee754fc285ba15
Eitan Adler <lists@eitanadler.com>
2025-02-05 05:16:49 -0800

    [repo] correct markdownlint globs, and updtae changed-files

--- go.zsh
+++ go.zsh
@@ -2,7 +2,7 @@
 
 hugo version
 pre-commit run --all
-markdownlint-cli2 --config .markdownlint-cli2.jsonc 'content/**.md'
+markdownlint-cli2 --config .markdownlint-cli2.jsonc 'content/**/**.md'
 vale content
 # https://gohugo.io/troubleshooting/audit/
 HUGO_MINIFY_TDEWOLFF_HTML_KEEPCOMMENTS=true HUGO_ENABLEMISSINGTRANSLATIONPLACEHOLDERS=true hugo && grep -inorE "<\!-- raw HTML omitted -->|ZgotmplZ|\[i18n\]|\(<nil>\)|(&lt;nil&gt;)|hahahugo" public/
