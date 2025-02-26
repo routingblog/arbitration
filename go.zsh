c2945469e4846581939dc72cbbea3dd48e90994a
Eitan Adler <lists@eitanadler.com>
2025-02-26 12:22:08 -0800

    [hugo] bump to latest hugo: 0.145.0

--- go.zsh
+++ go.zsh
@@ -1,5 +1,12 @@
 #!/usr/bin/env zsh
 
+source .env
+true_hugo_version=$(hugo version | grep -E -o 'v([0-9.]+)' | grep -E -o '([0-9.]+)')
+if [[ "$HUGO_VERSION" != "$true_hugo_version" ]]
+then
+  print -P '%F{red}Hugo Version Mismatch\n%F{reset}'
+fi
+
 hugo version
 pre-commit run --all-files
 markdownlint-cli2 --config .markdownlint-cli2.jsonc 'content/**/**.md'
